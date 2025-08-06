7e57440412eb266d421f681434ddba791e319ff9
HimalThapaMagar <himalthapamagar@proton.me>
2025-08-06 09:28:34 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# euclidv2
+
+This will be the beginning of the EUCLID V2 with go language implemenation for the backend service.
---
+++ android/app/src/main/kotlin/com/example/euclidv2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.euclidv2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
